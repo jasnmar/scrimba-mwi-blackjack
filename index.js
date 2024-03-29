6f3ab0f7bda4969c76b45b6dda1964457c5384e3
jasnmar <jasnmar@gmail.com>
2024-03-29 11:27:51 -0400

    Making response a variable

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 let firstCard = 10
-let secondCard = 12
-let sum = firstCard + secondCard
+let secondCard = 10
+let sum = firstCard + secondCard 
 let hasBlackJack = false
-// 1. Create a variable called isAlive and assign it to true
-let isAlive = true;
+let isAlive = true
+// 1. Declare a variable called message and assign its value to an empty string
+let message;
 
-// 2. Flip its value to false in the appropriate code block 
+// 2. Reassign the message variable to the string we're logging out
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳";
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
-    isAlive = false;
+    message = "You're out of the game! 😭";
+    isAlive = false
 }
 
-// 3. Log it out to check that you're doing it right
-console.log(isAlive)
-
-console.log(4 === 3)  // false
-console.log(5 > 2)    // true
-console.log(12 > 12)  // false
-console.log(3 < 0)    // false
-console.log(3 >= 3)   //  true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   // false
+// 3. Log it out!
+console.log(message);
