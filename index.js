586ea385c3749ecbb3f24173733eb8145f75100e
jasnmar <jasnmar@gmail.com>
2024-03-29 11:44:46 -0400

    Start Game Button

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ let isAlive = true
 // 1. Declare a variable called message and assign its value to an empty string
 let message;
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false
+function setupPage(){
+    const sgBtn = document.getElementById('startGame');
+    sgBtn.addEventListener("click", startGame);
 }
+setupPage();
 
-// 3. Log it out!
-console.log(message);
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+function startGame(event) {
+    event.preventDefault();
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)
+}
